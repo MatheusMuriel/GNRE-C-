641566011d9d422028210d214a9c271df57727d6
Muriel <matheus.muriel@outlook.com>
2019-11-05 12:34:59 -0300

    Autenticação SSL

---
+++ ConsumoGNRE/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" 
+    CodeBehind="Default.aspx.cs" Inherits="ConsumoGNRE._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+        <h1>...</h1>
+        <asp:Button ID="Button1" class="btn btn-primary btn-lg" runat="server" Text="Teste" OnClick="Button1_Click" />
+    </div>
+
+</asp:Content>
---
+++ ConsumoGNRE/Default.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsumoGNRE
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Literal1.Text = enviarRequisicao();
+
+            //Instanciando web service
+            //ServiceReference1.GnreConfigUFSoapClient ws = new ServiceReference1.GnreConfigUFSoapClient();
+            ServiceReference3.GnreConfigUFSoapClient ws = new ServiceReference3.GnreConfigUFSoapClient();
+
+            X509Certificate2 cert = new X509Certificate2(@"C:\Certs\certBase251.pfx", "1234");
+            ws.ClientCredentials.ClientCertificate.Certificate = cert;
+
+            // HEAD //
+            //ServiceReference1.gnreCabecMsg head = new ServiceReference1.gnreCabecMsg();
+            ServiceReference3.gnreCabecMsg head = new ServiceReference3.gnreCabecMsg();
+            head.versaoDados = "1.00";
+            // HEAD //
+
+
+            // BODY //
+            XDocument xmlDados = XDocument.Parse(@"<TConsultaConfigUf xmlns=""http://www.gnre.pe.gov.br""><ambiente>2</ambiente><uf>AC</uf><receita courier=""N"">100048</receita></TConsultaConfigUf>");
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(xmlDados.CreateReader());
+
+            XmlNode dados = xDoc;
+            // BODY 
+            
+            // Fazendo a chamada
+            XmlNode result = ws.consultar(head, dados);
+
+            XDocument resultXDoc = XDocument.Parse(result.OuterXml);
+            //XmlDocument resultXmlDoc = new XmlDocument();
+            //resultXmlDoc.Load(resultXDoc.CreateReader());
+
+            //resultXDoc.Save(@"C:\A_xml_GNRE\return.xml");
+
+            Literal1.Text = DateTime.Now.ToString("h:mm:ss") + "           ::           " + resultXDoc.ToString();
+
+            Console.WriteLine("Fim");
+
+        }
+
+        private static string enviarRequisicao()
+        {
+            try
+            {
+                
+                X509Certificate2 cert = new X509Certificate2(@"C:\Certs\certBase251.pfx", "1234");
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+                HttpWebRequest request = CreateWebRequest();
+
+                XDocument xmlDados = XDocument.Parse(@"<soap:Envelope xmlns:soap=""http://www.w3.org/2003/05/soap-envelope"" xmlns:gnr=""http://www.gnre.pe.gov.br/webservice/GnreConfigUF""><soap:Header><gnr:gnreCabecMsg><gnr:versaoDados>2.00</gnr:versaoDados></gnr:gnreCabecMsg></soap:Header><soap:Body><gnr:gnreDadosMsg><TConsultaConfigUf xmlns=""http://www.gnre.pe.gov.br""><ambiente>2</ambiente><uf>AC</uf><receita courier=""N"">100048</receita></TConsultaConfigUf></gnr:gnreDadosMsg></soap:Body></soap:Envelope>");
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlDados.CreateReader());
+
+                InsertSoapEnvelopeIntoWebRequest(xmlDoc, request);
+                request.ClientCertificates.Add(cert);
+
+                using (HttpWebResponse resposta = request.GetResponse() as HttpWebResponse)
+                {
+                    using (var stream = resposta.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            var result = reader.ReadToEnd();
+                            return result;
+                        }
+                    }
+                }
+            }
+            catch (WebException webex)
+            {
+                WebResponse errResp = webex.Response;
+                using (Stream respStream = errResp.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(respStream);
+                    string text = reader.ReadToEnd();
+                    return text;
+                }
+            }
+        }
+        public static HttpWebRequest CreateWebRequest()
+        {
+            String url = @"http://www.testegnre.pe.gov.br/gnreWS/services/GnreConfigUF";
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Headers.Add(@"SOAPAction", @"http://www.testegnre.pe.gov.br/gnreWS/services/GnreConfigUF/consultar");
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
+        private static void InsertSoapEnvelopeIntoWebRequest(XmlDocument soapEnvelopeXml, HttpWebRequest webRequest)
+        {
+            using (Stream stream = webRequest.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+        }
+    }
+}
